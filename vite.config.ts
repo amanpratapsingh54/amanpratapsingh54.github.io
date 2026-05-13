d4b7153d4e4cc09e6a74b5d8c7cd4e2934951661
amanpratapsingh54 <97aman@gmail.com>
2026-05-13 13:03:57 -0400

    Fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ const base = process.env.VITE_BASE_PATH ?? (repoName ? `/${repoName}/` : "/");
 
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/advanced-ai-portfolio/',
 });
